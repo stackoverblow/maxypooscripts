aaf53290e956fa82061d3fa2a603afcdd627d85e
stackoverblow <maximusjdhalloran@gmail.com>
2025-11-07 17:42:20 -0500

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,54 +1,12 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local v0=loadstring(game:HttpGet("https://sirius.menu/rayfield"))();local v1=v0:CreateWindow({Name="maxypoo hub",Icon=0,LoadingTitle="maxypoo scripts",LoadingSubtitle="by maxypoo",ShowText="Rayfield",Theme="Default",ToggleUIKeybind="K",DisableRayfieldPrompts=false,DisableBuildWarnings=false,ConfigurationSaving={Enabled=true,FolderName=nil,FileName="maxypooscripts"},Discord={Enabled=false,Invite="EMU5g5M8",RememberJoins=true},KeySystem=true,KeySettings={Title="Key System",Subtitle="Key System",Note="Join our Discord server for a key.",FileName="maxypookey",SaveKey=true,GrabKeyFromSite=true,Key={"Xeno"}}});local v2=v1:CreateTab("scripts","scroll-text");Tab:CreateButton({Name="Join our Discord!",Callback=function() game.Players.LocalPlayer:PromptOpenURL("https://discord.gg/EMU5g5M8");end});Tab:CreateButton({Name="Dress to Impress",Callback=function() pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/hellohellohell012321/DTI-GUI-V2/main/dti_gui_v2.lua",true))();end);end});
+-- ⚠️ WARNING: integrity protected!
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.10.9) ~  Much Love, Ferib 
 
-local Window = Rayfield:CreateWindow({
-    Name = "maxypoo hub",
-    Icon = 0,
-    LoadingTitle = "maxypoo scripts",
-    LoadingSubtitle = "by maxypoo",
-    ShowText = "Rayfield",
-    Theme = "Default",
-
-    ToggleUIKeybind = "K",
-
-    DisableRayfieldPrompts = false,
-    DisableBuildWarnings = false,
-
-    ConfigurationSaving = {
-        Enabled = true,
-        FolderName = nil,
-        FileName = "maxypooscripts"
-    },
-
-    Discord = {
-        Enabled = false,
-        Invite = "EMU5g5M8",
-        RememberJoins = true
-    },
-
-    KeySystem = true,
-    KeySettings = {
-        Title = "Key System",
-        Subtitle = "Key System",
-        Note = "Join our Discord server for a key.", -- Use this to tell the user how to get a key
-        FileName = "maxypookey", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-        SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-        GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-        Key = {"Xeno"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-    }
-})
-
-local Scripts = Window:CreateTab("scripts", "scroll-text")
-Tab:CreateButton({
-    Name = "Join our Discord!",
-    Callback = function()
-        game.Players.LocalPlayer:PromptOpenURL("https://discord.gg/EMU5g5M8")
-    end,
-})
-Tab:CreateButton({
-    Name = "Dress to Impress",
-    Callback = function()
-        pcall(function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/hellohellohell012321/DTI-GUI-V2/main/dti_gui_v2.lua",true))()
-        end)
-    end,
-})
+]]--
